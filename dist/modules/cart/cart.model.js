974a68772ad98eabd0b494cc5c149aa2ac923cd7
Al Amin Islam Atif <97211464+atif727@users.noreply.github.com>
2024-08-29 04:34:14 +0600

    Delete dist directory

--- dist/modules/cart/cart.model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.cartModel = void 0;
-const mongoose_1 = require("mongoose");
-const cartSchema = new mongoose_1.Schema({
-    userEmail: { type: String, required: true },
-    prodIds: { type: [String], default: [] },
-}, { versionKey: false });
-exports.cartModel = (0, mongoose_1.model)("cart", cartSchema);
--- dist/modules/products/product.constant.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.productSearchAbleFields = void 0;
-exports.productSearchAbleFields = [
-    "_id",
-    "name",
-    "category",
-    "price"
-];
